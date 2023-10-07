f0b7d9e194e5e895403ec1eb762976226909bac6
hmtmcse <hmtm.cse@gmail.com>
2023-10-07 20:50:14 +0600

    working on cli and template

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_dependencies():
 
 setup(
     name='pweb-auth',
-    version='1.0.0',
+    version='0.0.1',
     url='https://github.com/problemfighter/pweb-auth',
     license='Apache 2.0',
     author='Problem Fighter',
